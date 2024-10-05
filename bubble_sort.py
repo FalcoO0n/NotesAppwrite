7324de990257f766a98b514d8d4ba701c1839e62
Ruchika Sarode <ruchikasarode@Ruchikas-MacBook-Air.local>
2024-10-05 11:22:22 +0530

    test mac

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,7 +1,10 @@
 def bubble_sort(list_):
     n = len(list_)
-    for i in range(n-1):
-        for j in range(n-i-1):
-            if list_[j] > list_[j+1]:
-                list_[j], list_[j+1] = list_[j+1], list_[j]
-
+    for i in range(n - 1):
+        swapped = False
+        for j in range(n - i - 1):
+            if list_[j] > list_[j + 1]:
+                list_[j], list_[j + 1] = list_[j + 1], list_[j]
+                swapped = True
+        if not swapped:
+            break
